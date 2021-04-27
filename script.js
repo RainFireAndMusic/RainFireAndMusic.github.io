acfd85b86f88085346a9638d242932ba491a8ce0
rev4587 <hikaru975@gmail.com>
2021-04-27 16:19:15 -0400

    ?

--- script.js
+++ script.js
@@ -71,14 +71,14 @@
 	var selectedChannelIndex = 0;
 
 	// Find first live stream
-	embed.addEventListener(Twitch.Embed.VIDEO_READY, function() {
+	musicPlayer.addEventListener(Twitch.Player.OFFLINE, function() {
 		var pastCurrentChannel = false;
 		var shouldBailOutOfLoop = false;
 		$.each(channels, function(genreIndex, channelsInGenre){
 			$.each(channelsInGenre, function(channelIndex, channel){
 				if(channel[0] == musicPlayer.getChannel()){
 					pastCurrentChannel = true;
-				}else if(pastCurrentChannel && musicPlayer.getQualities().length == 0){
+				}else if(pastCurrentChannel){
 					console.log("setting channel to: " + channel[0]);
 					setTwitchChannel(channel[0]);
 					shouldBailOutOfLoop = true;
